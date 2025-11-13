5596c100131f6d06745929caf40c4847dbf8a401
Ot Roca <otrocadev@gmail.com>
2025-11-13 10:56:42 +0100

    feat: add secondary font family and create films search component

---
+++ src/app/films-search/films-search.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilmsSearchComponent } from './films-search.component';
+
+describe('FilmsSearchComponent', () => {
+  let component: FilmsSearchComponent;
+  let fixture: ComponentFixture<FilmsSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FilmsSearchComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FilmsSearchComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/films-search/films-search.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-films-search',
+  imports: [],
+  templateUrl: './films-search.component.html',
+  styleUrl: './films-search.component.css'
+})
+export class FilmsSearchComponent {
+
+}
