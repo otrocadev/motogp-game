4cc373bdad601a22e86c9b3612d0d15b187e3f4a
Ot Roca <otrocadev@gmail.com>
2025-11-20 11:25:35 +0100

    Remove environment files from tracking

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  SUPABASE_URL: process.env['SUPABASE_URL'],
-  SUPABASE_KEY: process.env['SUPABASE_KEY'],
-  OMDB_API_KEY: process.env['OMDB_API_KEY'],
-  OMDB_API_URL: process.env['OMDB_API_URL'],
-  SESSION_ACTIVE_DEFAULT: false,
-};
