c07ae906d86b73c2e759c07b991fca3ee54e57f0
Ot Roca <otrocadev@gmail.com>
2025-12-08 17:26:58 +0100

    feat: add user home summary page, create home navigation menu item with icon, and set home as default landing page for user view

---
+++ src/app/home-page/user-home/user-home-summary-page/user-home-summary-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-home-summary-page',
+  imports: [],
+  templateUrl: './user-home-summary-page.component.html',
+})
+export class UserHomeSummaryPageComponent {}
